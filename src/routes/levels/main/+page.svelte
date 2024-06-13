b386891fdf864da07edb753a7abea7a8110f939d
TyronVT <tyrontalusan00@gmail.com>
2024-06-13 16:06:47 +0800

    Fixed main list having all levels

--- src/routes/levels/main/+page.svelte
+++ src/routes/levels/main/+page.svelte
@@ -6,10 +6,8 @@
     
     const pb = new Pocketbase(SERVER_URL);
     async function getLevels() {
-        const records = await pb.collection('level_ranks_and_points').getFullList({
-            sort: 'level_rank_int'
-        });
-        return records;
+        return fetch(`${SERVER_URL}/api/levels/main`)
+        .then(response => response.json());
     }
 </script>
 
@@ -17,7 +15,7 @@
 
 <div class="flex flex-col items-center gap-8 pt-8">
     {#await getLevels()}
-        <p>Fetching players ...</p>
+        <p>Fetching levels ...</p>
     {:then data}
         {#each data as level, index}
             <Level levelName={level.level_name} levelRank={level.level_rank_int} levelPoints={level.level_points} />
