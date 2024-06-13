437d116cca0273d449a11ab51391028a08cead01
TyronVT <tyrontalusan00@gmail.com>
2024-06-13 15:18:35 +0800

    Removed example email (commented out)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -294,7 +294,7 @@
             </a>
           </li>
         </ul>
-        <div class="mt-auto flex">
+        <!-- <div class="mt-auto flex">
           <div class="flex w-full justify-between">
             <span class="text-sm font-medium text-black dark:text-white">email@example.com</span>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-roledescription="more menu" fill="none" stroke="currentColor" stroke-width="2" class="h-5 w-5 text-black dark:text-white lucide lucide-more-horizontal" stroke-linecap="round" stroke-linejoin="round">
@@ -303,7 +303,7 @@
               <circle cx="5" cy="12" r="1" />
             </svg>
           </div>
-        </div>
+        </div> -->
       </div>
     </aside>
 </div>
