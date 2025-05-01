87c4925b84b5f6a74e309e32970971ee1688ffdd
TyronVT <tyrontalusan00@gmail.com>
2025-05-01 15:52:59 +0800

    Removed notable progress in mobile sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -217,15 +217,6 @@
                 <span class="ml-3 flex-1 whitespace-nowrap">Legacy List</span>
                 </a>
 
-                <a href="/progress" class="flex items-center rounded-lg px-6 py-2 text-slate-900 hover:bg-slate-100 dark:text-white dark:hover:bg-slate-700">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 lucide lucide-home" width="24" height="24" aria-hidden="true" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                    <circle cx="9" cy="7" r="4" />
-                    <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-                    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                </svg>
-                <span class="ml-3 flex-1 whitespace-nowrap">Notable Progress</span>
-                </a>
             </ul>
 
           </li>
